0eeed1722f0aca61896786403cbb29dcafc1444b
Chip <74501847+Revolutionno@users.noreply.github.com>
2024-07-28 18:29:02 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 Config = {}
-Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
-Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
-Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
+Config.Interior = vector3(-1004.36, -477.9, 51.63) -- Interior to load where characters are previewed
+Config.DefaultSpawn = vector3(-1004.36, -477.9, 51.63) -- Default spawn coords if you have start apartments disabled
+Config.PedCoords = vector4(-1008.49, -474.51, 50.03, 208.64) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-1001.11, -478.06, 50.03, 24.55) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(-1005.53, -480.73, 50.52, 27.44) -- Camera coordinates for character preview screen
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
-Config.customNationality = false -- Defines if Nationality input is custom of blocked to the list of Countries
+Config.customNationality = false -- Defines if Nationality input is custom of blocked to the list of Countries !!!(Don't touch this if you don't know what you are doing)!!!
 Config.SkipSelection = false -- Skip the spawn selection and spawns the player at the last location
 
 Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
